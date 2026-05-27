99cce08ef26a08f999b73b51db50aa3c6cd1fe81
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 08:12:47 +0100

    feat: implement module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "association_id" {
+  description = "The ID of the VPC Endpoint Route Table Association."
+  value       = try(aws_vpc_endpoint_route_table_association.this[0].id, "")
+}
+
 output "enabled" {
   description = "Whether the module is enabled."
   value       = local.enabled
